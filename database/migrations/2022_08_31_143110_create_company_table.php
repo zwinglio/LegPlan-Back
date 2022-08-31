7bf8d053ee4a1a4d01962ef6c14503adfe474c96
Samuel Zwinglio <samuelzwinglio@gmail.com>
2022-08-31 13:07:55 -0300

    create company controller, model, database and seed

---
+++ database/migrations/2022_08_31_143110_create_company_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('mission');
+            $table->string('vision');
+            $table->string('values');
+            $table->date('plan_start_date')->nullable();
+            $table->date('plan_end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+};
