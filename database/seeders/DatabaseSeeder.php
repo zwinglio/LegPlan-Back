d66574a6b667e6d4bd4fc4882d23eae88a23943a
Samuel Zwinglio <samuelzwinglio@gmail.com>
2023-01-06 13:43:03 -0300

    insert task to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             ObjectiveSeeder::class,
             InitiativeSeeder::class,
             ActionSeeder::class,
+            TaskSeeder::class,
         ]);
     }
 }
