84213287e294ee794c0bcb3885f7303e70b710f9
Samuel Zwinglio <samuelzwinglio@gmail.com>
2023-03-07 14:44:27 -0300

    start user controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ActionController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ObjectiveController;
@@ -33,6 +34,8 @@ Route::get('/', function () {
 
 Route::get('/company', [CompanyController::class, 'index']);
 Route::put('/company', [CompanyController::class, 'update']);
+
+Route::apiResource('/users', UserController::class);
 
 Route::apiResource('/departments', DepartmentController::class);
 
